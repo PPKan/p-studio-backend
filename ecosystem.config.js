fbe31608e60b5178871e2222ef095b4296a4a34f
Peter Work Laptop <petervsjim@gmail.com>
2022-08-16 10:22:15 +0800

    configur pm2 and its post-deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "strapi"
+    },
+  ],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "ec2-18-182-56-247.ap-northeast-1.compute.amazonaws.com",
+      ref: "origin/main",
+      repo: "git@github.com:PPKan/p-studio-backend.git",
+      path: "/home/ubuntu/backend",
+      "post-deploy":
+        "npm install && NODE_ENV=production npm run build && pm2 reload ~/ecosystem.config.js",
+    },
+  },
+};
