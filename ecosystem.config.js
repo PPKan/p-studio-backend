ca823193dcf8a702450097deb3b0ce323abea6c3
Peter Work Laptop <petervsjim@gmail.com>
2022-08-16 11:21:29 +0800

    setup pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       key: "~/.ssh/0813-p-studio.pem",
       ref: "origin/main",
       repo: "git@github.com:PPKan/p-studio-backend.git",
-      path: "/home/ubuntu/backend",
+      path: "/home/ubuntu/p-studio-backend",
       "post-deploy":
         "nvm use 16.14.2 && npm install && NODE_ENV=production npm run build && pm2 reload ~/ecosystem.config.js",
     },
